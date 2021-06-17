f2dbed32451507007474f62feffcbcc414095c65
PretziDizzy <preetidas_a@yahoo.com>
2021-06-16 21:12:18 -0500

    First commit

---
+++ src/main/java/com/example/FormSubmission/FormSubmissionApplication.java
@@ -0,0 +1,14 @@
+package com.example.FormSubmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormSubmissionApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FormSubmissionApplication.class, args);
+	}
+
+}
